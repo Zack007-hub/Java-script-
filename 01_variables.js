f3b350d7fbbed48bcbdb0a813c1b83671d41d2cd
Zack007-hub <zakiahmad1702@gmail.com>
2026-03-31 02:52:33 +0500

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const accountId= 123345
+let accountEmail="Zaki@gmail.com"
+var accountPassword="1234"
+accountCity="Islamabad"
+
+//accountId=234
+accountEmail="Ahmad@gmail.com"
+accountPassword= 2212121
+accountCity="Lahore"
+//only constant cannot be change whereas var and let can be change
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
