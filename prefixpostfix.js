0865c9b0c88a3a1c1452ff122e22dabe5d428eaf
Zack007-hub <zakiahmad1702@gmail.com>
2026-03-31 21:44:04 +0500

    Add files via upload

---
+++ prefixpostfix.js
@@ -0,0 +1,15 @@
+//******************Postfix***************//
+let x=2
+const y= x++
+console.log(`x:${x},y:${y}`);
+//x++ means 
+//Use the current value of x,then increment it
+//y gets the current value whereas the value of x is incremented
+
+//*****************Prefix******************//
+let a=4
+const b = ++a
+console.log(`a:${a},b:${b}`);
+//++x means
+//increment a first then use the new value
+// a is incremented and b gets the updated value
