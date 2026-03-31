8a4ef2942f8663cccaa7babcde340c7e33ce4eeb
Zack007-hub <zakiahmad1702@gmail.com>
2026-03-31 20:04:17 +0500

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newer version
+//alert(3 + 3);//we are using node.js not browser
+console.log(3
+    +
+    3)//code readability should be high
+console.log("Zaki")
+let name = "Zaki"
+let age = 18
+let isLoggedIn = false
+let state;
+//number => 2 to the power of 53
+//bigint
+//string = ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof "Zaki");//string
+console.log(typeof 18);//number
+console.log(typeof null);//object
+console.log(typeof true);//boolean
+console.log(typeof undefined);//undefined
